bb633a2e7a6573ff82f2c4ea97aba13615c0cbbb
William <william.sarivan@solcentrix-group.com>
2024-10-27 23:54:56 +0200

    add footer web component

---
+++ src/components/footer.js
@@ -0,0 +1,33 @@
+class AppFooter extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        this.shadowRoot.innerHTML = `
+            <footer style="display: flex; flex-direction: column; align-items: center; background-color: #333; color: white; padding: 1em;">
+                <div style="display: flex; gap: 1em;">
+                    <lion-icon .icon="twitter" @click="${this.handleIconClick('https://twitter.com')}"></lion-icon>
+                    <lion-icon .icon="discord" @click="${this.handleIconClick('https://discord.com')}"></lion-icon>
+                    <lion-icon .icon="facebook" @click="${this.handleIconClick('https://facebook.com')}"></lion-icon>
+                </div>
+                <nav style="margin-top: 1em; display: flex; gap: 2em;">
+                    <a href="#careers" style="color: white;">Careers</a>
+                    <a href="#about" style="color: white;">About</a>
+                    <a href="#terms" style="color: white;">Terms and Conditions</a>
+                </nav>
+            </footer>
+        `;
+    }
+
+    handleIconClick(url) {
+        window.open(url, '_blank');
+    }
+}
+
+customElements.define('app-footer', AppFooter);
