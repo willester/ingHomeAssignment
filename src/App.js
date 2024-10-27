68a8f61e921287fc7d7756d07b440bfbd63672c4
William <william.sarivan@solcentrix-group.com>
2024-10-28 00:00:23 +0200

    change App.js to apply the created web components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import './AppHeader';
+import './AppFooter';
+import './WaveAnimation';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    useEffect(() => {
+        // Register the custom elements when the component mounts
+        const appHeader = document.createElement('app-header');
+        document.body.insertBefore(appHeader, document.getElementById('main-content'));
+
+        const appFooter = document.createElement('app-footer');
+        document.body.appendChild(appFooter);
+
+        const waveAnimation = document.createElement('wave-animation');
+        document.body.insertBefore(waveAnimation, appFooter);
+    }, []);
+
+    return (
+        <div>
+            <div id="main-content">
+                {/* Main content will be dynamically replaced */}
+            </div>
+        </div>
+    );
+};
 
 export default App;
